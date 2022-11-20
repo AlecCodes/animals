5e2515546c80dabcecaf6623919a2c117e6c5a69
Alec Hannaford <alec322@gmail.com>
2022-11-20 15:36:52 -0800

    adding globals and middleware

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express')
 require('dotenv').config()
+const morgan = require('morgan')
+const PORT = process.env.PORT
+
+const app = express()
+
+
+/////////////////////////////
+// Middleware
+/////////////////////////////
+app.use(morgan("tiny"))
+
+
+app.listen(PORT, ()=> console.log(`ANIMAL WORLD listening at ${PORT}`))
