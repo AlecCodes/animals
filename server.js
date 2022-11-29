7d2302009bb7ebf0cecbf06c75bc6653e5aa68e1
Alec Hannaford <alec322@gmail.com>
2022-11-28 17:40:42 -0800

    adding user login

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ const {Schema, model} = mongoose
 const animalSchema = new Schema({
     name: String,
     color: String,
-    isScary: Boolean
+    isScary: Boolean,
+    lifeExpectancy: Number
 })
 
 const Animal = model("animal",animalSchema)
@@ -56,18 +57,75 @@ const Animal = model("animal",animalSchema)
 //SEED route - Empty and populate database w seed data
 app.get("/animals/seed",(req,res) =>{
     const startAnimals =[
-        {name: "Elephant", color:"gray", isScary: false},
-        {name: "Scorpion", color:"brown", isScary: true},
-        {name: "Brown Bear", color:"brown", isScary: true}
+        {name: "Elephant", color:"gray", isScary: false, lifeExpectancy: 48},
+        {name: "Scorpion", color:"brown", isScary: true, lifeExpectancy: 3},
+        {name: "Brown Bear", color:"brown", isScary: true, lifeExpectancy: 25}
     ]
     Animal.deleteMany({},(err,data) => {
         Animal.create(startAnimals,(err,createdAnimals) => {
             res.json(createdAnimals)
         })
     })
-
 })
 
+//Index route 
+app.get("/animals/",(req,res)=>{
+    //Get all animals from Mongo
+    Animal.find({})
+    .then((animals)=>{
+        res.render("animals/index.ejs",{animals})
+    })
+})
+
+//NEW route
+app.get("/animals/new",(req,res)=>{
+    res.render("animals/new.ejs")
+})
+
+
+
+//DELETE Route
+app.delete("/animals/:id",(req,res)=>{
+    Animal.findByIdAndDelete(req.params.id,(err,deletedAnimal)=>{
+        console.log(err, deletedAnimal)
+        res.redirect('/animals')
+    })
+})
+
+//UPDATE ROUTE
+app.put("/animals/:id",(req,res)=>{
+    Animal.findByIdAndUpdate(req.params.id, req.body, {new:true},(error,updatedAnimal)=>{
+        res.redirect(`/animals/${req.params.id}`)
+    })
+})
+
+//CREATE route
+app.post("/animals",(req,res)=>{
+    req.body.isScary = req.body.isScary === 'on' ? true : false
+    Animal.create(req.body, (err, createdAnimal) =>{
+        res.redirect("/animals")
+        console.log(createdAnimal)
+    })
+})
+
+//EDIT route
+app.get("/animals/:id/edit",(req,res)=>{
+    Animal.findById((req.params.id),(err, foundAnimal)=>{
+        res.render('animals/edit.ejs',{foundAnimal})
+    })
+})
+
+
+//SHOW Route
+app.get("/animals/:index",(req,res)=>{
+    Animal.findById(req.params.index)
+    .then((animal)=>{
+        res.render('animals/show.ejs',{
+        animal:animal,
+        index:req.params.index
+        })
+    })
+})
 
 //LISTENER
 app.listen(PORT, ()=> console.log(`ANIMAL WORLD listening at ${PORT}`))
